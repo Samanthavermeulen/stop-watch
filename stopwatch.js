cc325c1791caf55bcd7e2f48efeed807f87532a4
Samanthavermeulen <s.vermeulen@caesar.nl>
2020-02-26 12:23:10 +0100

    added frontend for the stopwatch

--- stopwatch.js
+++ stopwatch.js
@@ -1,36 +1,41 @@
 // Constructor Function
 function Stopwatch(duration) {
-  // propety:
   startTime = duration; // keep the count of the start method
   let timeout;
-  // three methods:
+
   this.reset = function() {
     duration = startTime; // Sets the time back to the starting value
     return 'Reset';
-    // reset method can only reset when the start method is called first
   };
 
   this.start = function() {
-    // when the method start is called it can't not start again..
     if (duration >= 1) {
       timeout = setTimeout(sw.start, 1000, duration);
-      console.log(duration--);
+      duration--;
+      document.getElementById('result').innerHTML = duration;
     }
   };
 
   this.stop = function() {
-    // stop method can only stop when the start method is called first..
     clearTimeout(timeout);
     return 'Stop';
   };
 
-  // getter
+  this.read = function() {
+    duration = document.getElementById('num').value;
+    return 'read';
+  };
+
+  // getter and setter
   Object.defineProperty(this, 'duration', {
     get: function() {
       clearTimeout(timeout);
       return duration;
+    },
+    set: function(value) {
+      duration = value;
     }
   });
 }
 
-const sw = new Stopwatch(20);
+const sw = new Stopwatch();
